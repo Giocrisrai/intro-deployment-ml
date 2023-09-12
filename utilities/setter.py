a09440226c700cdc79bf16be51088c4279b340fb
Giocrisrai Godoy <giocrisrai@gmail.com>
2023-09-12 09:59:04 -0300

    Fix: setter.py again

--- utilities/setter.py
+++ utilities/setter.py
@@ -1,15 +1,10 @@
 import os
-from base64 import b64encode
+from base64 import b64decode
 
 def main():
     key = os.environ.get('SERVICE_ACCOUNT_KEY')
-    
-    # Decodifica la clave y luego codifica con base64
-    decoded_key = b64encode(key.encode()).decode()
-    
-    with open('path.json', 'w') as json_file:
-        json_file.write(decoded_key)
-
+    with open('path.json','w') as json_file:
+        json_file.write(b64decode(key).decode())
     print(os.path.realpath('path.json'))
 
 if __name__ == '__main__':
